720598915c7eafb65c32aeb76838fbe5792de65e
karma <karmatashi954@gmail.com>
2022-05-31 17:13:31 +0545

    Initial Commit

---
+++ CryptBro/CryptBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptBroApp.swift
+//  CryptBro
+//
+//  Created by karma on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
