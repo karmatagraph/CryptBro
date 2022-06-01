dcc30c33c73522ff68e0321c3325298e2807d131
karma <karmatashi954@gmail.com>
2022-06-01 09:54:48 +0545

    add of custom header view with animation

---
+++ CryptBro/Core/Home/Views/HomeView.swift
@@ -0,0 +1,61 @@
+//
+//  HomeView.swift
+//  CryptBro
+//
+//  Created by karma on 5/31/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            VStack {
+                homeHeaderView
+                Spacer()
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeaderView: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info" )
+                .animation(.none)
+                .background(CircleButtonAnimationView(animate: $showPortfolio))
+            Spacer()
+            Text("Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0 ))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
+}
---
+++ CryptBro/Extension/Color+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Color+Extension.swift
+//  CryptBro
+//
+//  Created by karma on 5/31/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextcolor")
+    
+}
